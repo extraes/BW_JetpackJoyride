5680ce64b2e6a6ab20f6bb61b2470b8ead603aa7
extraes <52384576+extraes@users.noreply.github.com>
2021-07-16 01:19:54 -0700

    Add files via upload

---
+++ MelonLoaderMod.cs
@@ -0,0 +1,55 @@
+﻿using MelonLoader;
+using ModThatIsNotMod;
+using ModThatIsNotMod.BoneMenu;
+using StressLevelZero.Props.Weapons;
+using StressLevelZero.Rig;
+using StressLevelZero.VFX;
+using StressLevelZero.VRMK;
+using System;
+using UnityEngine;
+
+namespace JetpackJoyride
+{
+    public static class BuildInfo
+    {
+        public const string Name = "Jetpack Joyride"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "extraes"; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class JetpackJoyride : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            var menu = MenuManager.CreateCategory("Jetpack Joyride", Color.grey);
+            menu.CreateBoolElement("Enabled/disabled", Color.yellow, false, new Action<bool>((bool b) =>
+            {
+                if (b)
+                {
+                    Hooking.OnPostFireGun += Hooking_OnPostFireGun;
+                }
+                else Hooking.OnPostFireGun -= Hooking_OnPostFireGun;
+            }));
+            menu.CreateBoolElement("Move only with guns", Color.yellow, false, new Action<bool>((bool b) =>
+            {
+                var rig = GameObject.FindObjectOfType<RigManager>();
+                if (rig != null)
+                {
+                    if (b) rig.ControllerRig.maxVelocity = 0;
+                    else rig.ControllerRig.maxVelocity = 2;
+                }
+            }));
+        }
+
+        private void Hooking_OnPostFireGun(Gun gun)
+        {
+            bool isPlayer = gun.transform.parent.gameObject.GetComponent<DespawnMeshVFX>() == null;
+            if (isPlayer)
+            {
+                GameObject.FindObjectOfType<PhysBody>()?.AddVelocityChange(-gun.transform.forward * 5);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(JetpackJoyride.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(JetpackJoyride.BuildInfo.Company)]
+[assembly: AssemblyProduct(JetpackJoyride.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + JetpackJoyride.BuildInfo.Author)]
+[assembly: AssemblyTrademark(JetpackJoyride.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(JetpackJoyride.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(JetpackJoyride.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(JetpackJoyride.JetpackJoyride), JetpackJoyride.BuildInfo.Name, JetpackJoyride.BuildInfo.Version, JetpackJoyride.BuildInfo.Author, JetpackJoyride.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
